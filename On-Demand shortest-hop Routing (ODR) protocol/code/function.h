f2dba19a8641888d1083e76984d032b6462d4239
cochung <cochung1993@gmail.com>
2017-02-08 21:58:39 -0500

    code

--- On-Demand shortest-hop Routing (ODR) protocol/code/function.h
+++
@@ -1,9 +0,0 @@
-void msg_send(int, char *, int, char *, int);
-
-char *msg_recv(int, int);
-
-void sayhi();
-
-void msg_send2(int, char *, int, char *, int, char *);
-
-void printReceivingMsg(char *, char *, char *);
